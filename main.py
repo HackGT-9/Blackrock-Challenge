065fe87ab8b530f0e9f5ca71dc9eb2cb14820fa3
Hakan "John" Gunerli <33205097+hakancangunerli@users.noreply.github.com>
2022-10-22 10:33:01 -0400

    i just moved it as public and private sector

--- main.py
+++ main.py
@@ -5,23 +5,21 @@ st.title("Financial Literacy: Explained")
 st.sidebar.title("Navigation")
 st.sidebar.markdown("Navigate to the page you want to visit")
 
-page = st.sidebar.selectbox("Select a page", ["Home", "Private Sector", "Public Sector", "Government Sector"])
+page = st.sidebar.selectbox("Select a page", ["Home", "Private Sector", "Public/Government Sector"])
 
 
 
 # this would be much faster rather than caching the entire page.
 def main():
     if page == "Home":
-        st.write("Welcome to the home page")
+        st.write("Welcome to the Volatility Explained! This is a website that aims to explain the basics of financial literacy to the average person. We hope you enjoy your stay!")
     elif page == "Private Sector":
         st.title("Private Sector")
-        st.write("This is the about page")
-    elif page == "Government Sector":
-        st.title("Government Sector")
-        st.write("This is the contact page")
-    elif page == "Public Sector":
-        st.title("Public Sector")
-        st.write("This is the about page")
-        
+        st.write("Private sector is generally the businesses and the companies. They  play the moving joint of an economy, like investors and people like you!")
+    elif page == "Public/Government Sector":
+        st.write("Although public sector is used synonymously, with this one we are referring to the PEOPLE. This is us, the people who are affected by the market volatility with having the least capability to change it unless they act as the government.")
+        st.write("There is multiple ways that the government can influence the economy when the market volatility is high.")
+        # TODO: get a graph here https://seekingalpha.com/article/4470812-rising-interest-rates-matter-stock-market
+        # TODO: READ this to get some insight https://www.investopedia.com/articles/economics/11/how-governments-influence-markets.asp
 if __name__ == "__main__":
     main()
